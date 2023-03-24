931c0bda2810658997906dc15f68821b6781c149
Tyler Chartrand <tyler@thechartrands.com>
2023-03-23 19:13:27 -0600

    Update

--- BST.h
+++ BST.h
@@ -5,10 +5,25 @@
 #pragma once
 
 #include "PrintStuff.h"
+#include "BSTInterface.h"
 
-class BST {
+class BST : public BSTInterface {
 public:
     BST() {
         println("test?????");
     }
+
+    ~BSTInterface() = default;
+
+    Node *getRootNode() const = 0;
+
+    bool insert(int data) = 0;
+
+    bool remove(int data) = 0;
+
+    bool contains(int data) = 0;
+
+    void clear() = 0;
+
+
 };
